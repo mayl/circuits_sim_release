ee422daa36a15b33793fdf6193ce0aa297cb7729
Larry May <192939+mayl@users.noreply.github.com>
2025-08-26 18:26:08 -0400

    Fix with upstream branch

--- circuits_sim_test/mix.exs
+++ circuits_sim_test/mix.exs
@@ -22,8 +22,10 @@ defmodule CircuitsSimTest.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_i2c, "~> 2.1"},
+      #{:circuits_i2c, "~> 2.1"},
+      {:circuits_i2c, github: "elixir-circuits/circuits_i2c", branch: "dont-load-nif-if-not-built", override: true},
       {:circuits_sim, "~> 0.1"},
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
